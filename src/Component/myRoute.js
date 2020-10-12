d8a6e1c7e24779a6953907df31e5746664c4eb90
Yamin Alam Sarker <yaminalam27@gmail.com>
2020-10-12 14:22:16 +0600

    Not Found URL/Exception

--- src/Component/myRoute.js
+++ src/Component/myRoute.js
@@ -1,19 +1,27 @@
 import React, {Component} from 'react';
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import Home from "./Pages/Home";
 import Page1 from "./Pages/Page1";
 import Page2 from "./Pages/Page2";
 import Page3 from "./Pages/Page3";
+import NotFound from "./Pages/NotFound";
 
 class MyRoute extends Component {
     render() {
         return (
             <div>
 
-                <Route exact path="/" component={Home}/>
-                <Route exact path="/page1" component={Page1}/>
-                <Route exact path="/page2" component={Page2}/>
-                <Route exact path="/page3" component={Page3}/>
+                <Switch>
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/page1" component={Page1}/>
+                    <Route exact path="/page2" component={Page2}/>
+                    <Route exact path="/page3" component={Page3}/>
+
+                    <Route component={NotFound}/>
+
+                </Switch>
+
+
 
             </div>
         );
